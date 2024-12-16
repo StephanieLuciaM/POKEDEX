1cfb812ce6e9afc01ae320c7222bae7571835e7c
StephanieLuciaM <marie_lucia@hotmail.fr>
2024-12-16 23:40:12 +0100

    pokemon module

---
+++ back/controllers/pokemon.controller.js
@@ -0,0 +1,21 @@
+import { Pokemon } from "../models/associations.js";
+import { sequelize } from "../models/client.js";
+
+
+export const pokemonController ={
+    async getAll(req,res){
+        const pokemons =await Pokemon.findAll();
+        res.json(pokemons)
+    },
+
+    async getOne(req,res) {
+        const{id}= req.params;
+        const pokemon = await Pokemon.findByPk(id);
+        if(!pokemon){
+            return res.status(404).json({error: 'Pokemon not found'});
+            }
+        
+            res.json(pokemon);
+        },
+};
+
--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,6 +1,18 @@
 import { Router } from "express";
+import { pokemonController } from "../controllers/pokemon.controller.js";
+import { controllerWrapper } from "../middlewares/controller.wrapper.js";
 
 
 
 
 export const router = new Router();
+
+router.route('/pokemons')
+    .get(
+        controllerWrapper(
+        pokemonController.getAll
+    )
+    );
+
+    router.route('/pokemons/:id(\\d+)')
+    .get(controllerWrapper(pokemonController.getOne));

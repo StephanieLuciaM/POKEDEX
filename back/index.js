3a7bf0950e8f62b028321691f750814585c08aa5
StephanieLuciaM <marie_lucia@hotmail.fr>
2024-12-17 12:05:05 +0100

    affichage des pokemons

--- back/index.js
+++ back/index.js
@@ -4,6 +4,8 @@ import "dotenv/config";
 // Importer Express
 import express from "express";
 
+//sécutité
+import cors from "cors";
 
 // Import des modules locaux
 //Router
@@ -15,7 +17,11 @@ import { router } from "./routes/routes.js";
 // Créer une app
 const app = express();
 
-
+// module CORS : spécifier qui peut acceder à l'api
+app.use(cors({
+    // origin: ["monfront1.com, monfront2.fr"]
+    origin: "*" // * = autoriser tout le monde (pas une bonne pratique, mais en local, c'est tres bien !)
+  }));
 
 // BodyParser permettant d'interpréter des donénes fourni dans un POST, un PATCH ou un PUT, en tant que JSON. Ces données seront stockées dans req.body
 app.use(express.json());
